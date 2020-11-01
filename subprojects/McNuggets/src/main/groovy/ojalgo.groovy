a2d90f642fc2e6e26e308093cfb5e34b4f3f235e
Andres Almiray <aalmiray@gmail.com>
2020-11-01 12:17:46 +0100

    Fix kordamp settings Apply license headers

--- subprojects/McNuggets/src/main/groovy/ojalgo.groovy
+++ subprojects/McNuggets/src/main/groovy/ojalgo.groovy
@@ -1,3 +1,20 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2020 Paul King.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 //@Grab('org.ojalgo:ojalgo:48.3.1')
 import org.ojalgo.optimisation.ExpressionsBasedModel
 import static org.ojalgo.netio.BasicLogger.debug
